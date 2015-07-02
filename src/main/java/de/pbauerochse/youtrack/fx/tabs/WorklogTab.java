80acb3b0dc75d6349ad5f70bef09cd340bb6c21f
Patrick Bauerochse <pbauerochse@web.de>
2015-07-02 22:05:48 +0200

    small performance improvements

--- src/main/java/de/pbauerochse/youtrack/fx/tabs/WorklogTab.java
+++ src/main/java/de/pbauerochse/youtrack/fx/tabs/WorklogTab.java
@@ -9,6 +9,7 @@ import de.pbauerochse.youtrack.util.SettingsUtil;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
@@ -38,6 +39,8 @@ public abstract class WorklogTab extends Tab {
     protected SettingsUtil.Settings settings;
     protected TableView<TaskWithWorklogs> taskTableView;
 
+    protected ReportTimerange lastUsedTimerange;
+
     public WorklogTab(String name, ResourceBundle resourceBundle, SettingsUtil.Settings settings) {
         super(name);
         this.resourceBundle = resourceBundle;
@@ -66,7 +69,6 @@ public abstract class WorklogTab extends Tab {
 
     protected Node getTaskView() {
         taskTableView = new TableView<>();
-        taskTableView.getColumns().add(getDescriptionColumn());
 
         AnchorPane anchorPane = new AnchorPane(taskTableView);
         anchorPane.setPadding(new Insets(6));
@@ -80,78 +82,87 @@ public abstract class WorklogTab extends Tab {
     }
 
     public void updateItems(List<TaskWithWorklogs> worklogList, ReportTimerange timerange) {
-        // render tables for all days in the timerange
-        TimerangeProvider timerangeProvider = timerange.getTimerangeProvider();
-        long daysToDisplay = ChronoUnit.DAYS.between(timerangeProvider.getStartDate(), timerangeProvider.getEndDate());
 
-        for (int days = 0; days <= daysToDisplay; days++) {
-            LocalDate currentColumnDate = timerangeProvider.getStartDate().plus(days, ChronoUnit.DAYS);
-            DayOfWeek currentColumnDayOfWeek = currentColumnDate.getDayOfWeek();
-            String displayDate = FormattingUtil.formatDate(currentColumnDate);
+        if (lastUsedTimerange == null || lastUsedTimerange != timerange) {
+            taskTableView.getColumns().clear();
+            taskTableView.getColumns().add(getDescriptionColumn());
 
-            // create column
-            TableColumn<TaskWithWorklogs, TaskWithWorklogs> column = new TableColumn<>(displayDate);
-            column.setSortable(false);
-            column.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue()));
-            column.setCellFactory(param -> {
-                TableCell<TaskWithWorklogs, TaskWithWorklogs> cell = new TableCell<TaskWithWorklogs, TaskWithWorklogs>() {
-                    @Override
-                    protected void updateItem(TaskWithWorklogs item, boolean empty) {
-                        super.updateItem(item, empty);
+            // render tables for all days in the timerange
+            TimerangeProvider timerangeProvider = timerange.getTimerangeProvider();
+            long daysToDisplay = ChronoUnit.DAYS.between(timerangeProvider.getStartDate(), timerangeProvider.getEndDate());
 
-                        if (empty) {
-                            setText(StringUtils.EMPTY);
-                            setTooltip(null);
-                            getStyleClass().remove(SUMMARY_COLUMN_OR_CELL_CSS_CLASS);
-                        } else {
-                            String worklogTimeFormatted = FormattingUtil.formatMinutes(item.getTotalInMinutes(currentColumnDate));
+            for (int days = 0; days <= daysToDisplay; days++) {
+                LocalDate currentColumnDate = timerangeProvider.getStartDate().plus(days, ChronoUnit.DAYS);
+                DayOfWeek currentColumnDayOfWeek = currentColumnDate.getDayOfWeek();
+                String displayDate = FormattingUtil.formatDate(currentColumnDate);
 
-                            setText(worklogTimeFormatted);
-                            setTooltip(new Tooltip(displayDate + " - " + worklogTimeFormatted));
+                // create column
+                TableColumn<TaskWithWorklogs, TaskWithWorklogs> column = new TableColumn<>(displayDate);
+                column.setSortable(false);
+                column.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue()));
+                column.setCellFactory(param -> {
+                    TableCell<TaskWithWorklogs, TaskWithWorklogs> cell = new TableCell<TaskWithWorklogs, TaskWithWorklogs>() {
+                        @Override
+                        protected void updateItem(TaskWithWorklogs item, boolean empty) {
+                            super.updateItem(item, empty);
 
-                            if (item.isSummaryRow()) {
-                                getStyleClass().add(SUMMARY_COLUMN_OR_CELL_CSS_CLASS);
+                            if (empty) {
+                                setText(StringUtils.EMPTY);
+                                setTooltip(null);
+                                getStyleClass().remove(SUMMARY_COLUMN_OR_CELL_CSS_CLASS);
+                            } else {
+                                String worklogTimeFormatted = FormattingUtil.formatMinutes(item.getTotalInMinutes(currentColumnDate));
+
+                                setText(worklogTimeFormatted);
+                                setTooltip(new Tooltip(displayDate + " - " + worklogTimeFormatted));
+
+                                if (item.isSummaryRow()) {
+                                    getStyleClass().add(SUMMARY_COLUMN_OR_CELL_CSS_CLASS);
+                                }
                             }
                         }
-                    }
-                };
+                    };
 
-                cell.setAlignment(Pos.CENTER_RIGHT);
+                    cell.setAlignment(Pos.CENTER_RIGHT);
 
-                return cell;
-            });
+                    return cell;
+                });
 
-            // add class for weekend columns
-            if (currentColumnDayOfWeek == DayOfWeek.SATURDAY || currentColumnDayOfWeek == DayOfWeek.SUNDAY) {
-                column.getStyleClass().add(WEEKEND_COLUMN_OR_CELL_CSS_CLASS);
-                column.setPrefWidth(20);
-            } else {
-                column.setPrefWidth(100);
+                // add class for weekend columns
+                if (currentColumnDayOfWeek == DayOfWeek.SATURDAY || currentColumnDayOfWeek == DayOfWeek.SUNDAY) {
+                    column.getStyleClass().add(WEEKEND_COLUMN_OR_CELL_CSS_CLASS);
+                    column.setPrefWidth(20);
+                } else {
+                    column.setPrefWidth(100);
+                }
+
+                taskTableView.getColumns().add(column);
             }
 
-            taskTableView.getColumns().add(column);
-            taskTableView.getItems().clear();
-            taskTableView.getItems().addAll(worklogList);
+            // also add another summary per task column
+            TableColumn<TaskWithWorklogs, String> summaryPerTaskColumn = new TableColumn<>(resourceBundle.getString("view.main.summary"));
+            summaryPerTaskColumn.setSortable(false);
+            summaryPerTaskColumn.setCellValueFactory(param -> new SimpleStringProperty(FormattingUtil.formatMinutes(param.getValue().getTotalInMinutes())));
+            summaryPerTaskColumn.setCellFactory(param -> {
+                TableCell<TaskWithWorklogs, String> summaryCell = new TableCell<TaskWithWorklogs, String>() {
+                    @Override
+                    protected void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        setText(StringUtils.defaultIfBlank(item, StringUtils.EMPTY));
+                    }
+                };
+                summaryCell.getStyleClass().add(SUMMARY_COLUMN_OR_CELL_CSS_CLASS);
+                summaryCell.setAlignment(Pos.CENTER_RIGHT);
+                return summaryCell;
+            });
+            summaryPerTaskColumn.setPrefWidth(120);
+            taskTableView.getColumns().add(summaryPerTaskColumn);
+
+            lastUsedTimerange = timerange;
         }
 
-        // also add another summary per task column
-        TableColumn<TaskWithWorklogs, String> summaryPerTaskColumn = new TableColumn<>(resourceBundle.getString("view.main.summary"));
-        summaryPerTaskColumn.setSortable(false);
-        summaryPerTaskColumn.setCellValueFactory(param -> new SimpleStringProperty(FormattingUtil.formatMinutes(param.getValue().getTotalInMinutes())));
-        summaryPerTaskColumn.setCellFactory(param -> {
-            TableCell<TaskWithWorklogs, String> summaryCell = new TableCell<TaskWithWorklogs, String>() {
-                @Override
-                protected void updateItem(String item, boolean empty) {
-                    super.updateItem(item, empty);
-                    setText(StringUtils.defaultIfBlank(item, StringUtils.EMPTY));
-                }
-            };
-            summaryCell.getStyleClass().add(SUMMARY_COLUMN_OR_CELL_CSS_CLASS);
-            summaryCell.setAlignment(Pos.CENTER_RIGHT);
-            return summaryCell;
-        });
-        summaryPerTaskColumn.setPrefWidth(120);
-        taskTableView.getColumns().add(summaryPerTaskColumn);
+        // TODO render only if visible
+//        taskTableView.setItems(FXCollections.observableArrayList(worklogList));
     }
 
     protected Node getStatisticsView() {
