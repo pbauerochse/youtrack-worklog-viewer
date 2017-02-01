5955b05e17457720f73351597933eae9c2461726
Patrick Bauerochse <bauerochse@pondus.de>
2017-02-01 16:08:37 +0100

    IssueField Serializer (credit to @iileshii)

--- src/main/java/de/pbauerochse/worklogviewer/util/JacksonUtil.java
+++ src/main/java/de/pbauerochse/worklogviewer/util/JacksonUtil.java
@@ -2,7 +2,6 @@ package de.pbauerochse.worklogviewer.util;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -11,6 +10,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import de.pbauerochse.worklogviewer.youtrack.issuedetails.IssueField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -22,23 +23,10 @@ import java.io.Reader;
 public class JacksonUtil {
 
     private static ObjectMapper objectMapper = new ObjectMapper();
+
     static {
         SimpleModule customModule = new SimpleModule("IssueField", new Version(1, 0, 0, null, null, null))
-                .addDeserializer(IssueField.class, new JsonDeserializer<IssueField>() {
-                    @Override
-                    public IssueField deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
-                            throws IOException {
-                        ObjectCodec oc = jsonParser.getCodec();
-                        JsonNode node = oc.readTree(jsonParser);
-                        IssueField result = new IssueField();
-                        String name = node.get("name").asText();
-                        result.setName(name);
-                        JsonNode valueNode = node.get("value");
-                        String value = valueNode.isTextual() ? valueNode.asText() : "0";
-                        result.setValue(value);
-                        return result;
-                    }
-                });
+                .addDeserializer(IssueField.class, new IssueFieldDeserializer());
         objectMapper.registerModule(customModule);
     }
 
@@ -52,5 +40,28 @@ public class JacksonUtil {
 
     public static <T> T parseValue(Reader contentReader, TypeReference<T> typeReference) throws IOException {
         return objectMapper.readValue(contentReader, typeReference);
+    }
+
+    private static class IssueFieldDeserializer extends JsonDeserializer<IssueField> {
+
+        private static final Logger LOGGER = LoggerFactory.getLogger(IssueFieldDeserializer.class);
+
+        @Override
+        public IssueField deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+            JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+            JsonNode valueNode = node.get("value");
+
+            String name = node.get("name").asText();
+            String value = valueNode.isTextual() ? valueNode.asText() : "0";
+
+            if (!valueNode.isTextual()) {
+                LOGGER.info("Value JsonNode for field {} is not of type textual but of type {}", name, valueNode.getNodeType());
+            }
+
+            IssueField issueField = new IssueField();
+            issueField.setName(name);
+            issueField.setValue(value);
+            return issueField;
+        }
     }
 }
