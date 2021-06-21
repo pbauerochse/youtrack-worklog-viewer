a331a56e25dd360439300caf37886325651f0824
Patrick Brandes <zwoeinsrisiko@web.de>
2021-06-21 14:35:18 +0200

    Showing Issue fields in the details view

--- application/src/main/java/de/pbauerochse/worklogviewer/search/fx/details/IssueDetailsPane.kt
+++ application/src/main/java/de/pbauerochse/worklogviewer/search/fx/details/IssueDetailsPane.kt
@@ -2,6 +2,7 @@ package de.pbauerochse.worklogviewer.search.fx.details
 
 import de.pbauerochse.worklogviewer.fx.Theme
 import de.pbauerochse.worklogviewer.fx.issuesearch.WebViewSanitizer
+import de.pbauerochse.worklogviewer.search.fx.results.SearchResultIssueField
 import de.pbauerochse.worklogviewer.settings.SettingsUtil
 import de.pbauerochse.worklogviewer.tasks.Tasks
 import de.pbauerochse.worklogviewer.timereport.Issue
@@ -13,41 +14,37 @@ import javafx.event.EventHandler
 import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
 import javafx.scene.control.Label
-import javafx.scene.control.Tab
 import javafx.scene.control.TableView
-import javafx.scene.control.Tooltip
 import javafx.scene.layout.BorderPane
+import javafx.scene.layout.FlowPane
 import javafx.scene.web.WebView
 import org.slf4j.LoggerFactory
 import java.net.URL
 import java.util.*
 
-/**
- * Contains the details of an [de.pbauerochse.worklogviewer.timereport.Issue]
- */
-class IssueDetailsTab(private val issue: Issue) : Tab(issue.humanReadableId), Initializable {
+class IssueDetailsPane(private val issue: Issue): BorderPane(), Initializable {
 
-    private val workItems: ObservableList<WorkItem> = FXCollections.observableArrayList()
-
-    lateinit var issueDetailsPane: BorderPane
     lateinit var issueSummaryLabel: Label
+    lateinit var issueFieldsPane: FlowPane
     lateinit var issueDescriptionWebView: WebView
     lateinit var issueWorklogsTableView: TableView<WorkItem>
 
-    init {
-        isClosable = true
-        tooltip = Tooltip(issue.fullTitle)
+    private val workItems: ObservableList<WorkItem> = FXCollections.observableArrayList()
 
-        val loader = FXMLLoader(IssueDetailsTab::class.java.getResource("/fx/views/issue-details.fxml"))
-        loader.setController(this)
-        loader.load<BorderPane>()
+    init {
+        FXMLLoader(IssueDetailsPane::class.java.getResource("/fx/views/issue-details.fxml")).apply {
+            setController(this@IssueDetailsPane)
+            setRoot(this@IssueDetailsPane)
+        }.load<BorderPane>()
     }
 
-    override fun initialize(url: URL?, resource: ResourceBundle?) {
-        content = issueDetailsPane
+    override fun initialize(url: URL?, resourceBundle: ResourceBundle?) {
+        LOGGER.info("Preparing details pane for $issue")
         issueSummaryLabel.text = issue.fullTitle
         issueDescriptionWebView.engine.loadContent(WebViewSanitizer.sanitize(issue.description))
         issueWorklogsTableView.itemsProperty().bind(SimpleObjectProperty(workItems))
+        issue.fields.forEach { issueFieldsPane.children.add(SearchResultIssueField(it)) }
+
         SettingsUtil.settingsViewModel.themeProperty.addListener { _, _, newValue -> updateWebviewStyleSheet(newValue) }
 
         updateWebviewStyleSheet(SettingsUtil.settings.theme)
@@ -62,10 +59,10 @@ class IssueDetailsTab(private val issue: Issue) : Tab(issue.humanReadableId), In
     }
 
     private fun updateWebviewStyleSheet(theme: Theme) {
-        issueDescriptionWebView.engine.userStyleSheetLocation = IssueDetailsTab::class.java.getResource(theme.webviewStylesheet)?.toExternalForm()
+        issueDescriptionWebView.engine.userStyleSheetLocation = IssueDetailsPane::class.java.getResource(theme.webviewStylesheet)?.toExternalForm()
     }
 
     companion object {
-        private val LOGGER = LoggerFactory.getLogger(IssueDetailsTab::class.java)
+        private val LOGGER = LoggerFactory.getLogger(IssueDetailsPane::class.java)
     }
 }
