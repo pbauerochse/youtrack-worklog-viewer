7d3d1e1a861620afa386a877ac5f8e63faa78adb
Patrick Brandes <zwoeinsrisiko@web.de>
2021-07-22 12:10:42 +0200

    Long field values won't break the search result list view anymore

--- application/src/main/java/de/pbauerochse/worklogviewer/search/fx/results/SearchResultIssueField.kt
+++ application/src/main/java/de/pbauerochse/worklogviewer/search/fx/results/SearchResultIssueField.kt
@@ -4,6 +4,7 @@ import de.pbauerochse.worklogviewer.timereport.Field
 import javafx.geometry.Orientation
 import javafx.scene.control.Label
 import javafx.scene.control.Separator
+import javafx.scene.control.Tooltip
 import javafx.scene.layout.HBox
 
 /**
@@ -12,13 +13,20 @@ import javafx.scene.layout.HBox
 class SearchResultIssueField(field: Field) : HBox() {
 
     init {
+        val fieldValueText = field.value.joinToString().takeIf { it.isNotBlank() }
+
         styleClass.add("search-result-issue-field")
         children.addAll(
             Label("${field.name}:").apply {
                 styleClass.add("field-name")
             },
-            Label(field.value.joinToString().takeIf { it.isNotBlank() } ?: "-").apply {
+
+            Label(fieldValueText?.replace("\n", " ") ?: "-").apply {
                 styleClass.add("field-value")
+                isWrapText = false
+
+                maxWidth = 200.0
+                tooltip = fieldValueText?.let { Tooltip(it) }
             },
             Separator(Orientation.VERTICAL)
         )
